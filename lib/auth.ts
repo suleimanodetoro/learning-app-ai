50dc1ee3591ac75a59df5fd43172b9c847955a2b
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-22 17:09:17 +0100

    Prelim Next Auth Setup - bugs

---
+++ lib/auth.ts
@@ -0,0 +1,65 @@
+import { DefaultSession, NextAuthOptions } from "next-auth";
+import { prisma } from "./db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google"
+
+// because next auth does not know the shape of session defined in User, we are getting errors. Fix this by defining the followoing
+// extend the types of the next auth package
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      credits: number;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    credits: number;
+  }
+}
+
+export const authOption: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    jwt: async ({ token }) => {
+      // find the user in the database with a particular token
+      const db_user = await prisma.user.findFirst({
+        where: {
+          // where email is equal to token.email
+          email: token.email,
+        },
+      });
+      // if db user exists after check above, bind the user's id and credits to the token ->
+      if (db_user) {
+        token.id = db_user.id;
+        token.credits = db_user.credits;
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+        session.user.credits = token.credits;
+      }
+      return session;
+    },
+    
+  },
+  secret: process.env.NEXTAUTH_SECRET as string,
+    adapter: PrismaAdapter(prisma),
+    provider: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+
+    ]
+};
