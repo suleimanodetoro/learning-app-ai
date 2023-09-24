d6d488e2474d5d2dafe678f6531eacc22916a47d
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-24 18:41:11 +0100

    Add animation to form- on add and remove

--- components/CreateCourseForm.tsx
+++ components/CreateCourseForm.tsx
@@ -9,7 +9,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "./ui/input";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
-import { Minus, Plus, Trash } from "lucide-react";
+import { Plus, Trash } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 
 type Props = {};
 
@@ -53,32 +54,37 @@ const CreateCourseForm = (props: Props) => {
           />
 
           {/* Map out unit inputs */}
+          {/* Wrap in framer motion component to animate on form change */}
 
-          {form.watch("units").map((_, index) => {
-            return (
-              <FormField
-                key={index}
-                control={form.control}
-                name={`units.${index}`}
-                render={({ field }) => {
-                  return (
-                    <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
-                      <FormLabel className="flex-[1] text-xl ">
-                        Unit {index + 1}
-                      </FormLabel>
-                      <FormControl className="flex-[6]">
-                        <Input
-                          className="flex-[6]"
-                          placeholder="Enter subtopic of the main topic"
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  );
-                }}
-              />
-            );
-          })}
+          <AnimatePresence>
+            {form.watch("units").map((_, index) => {
+              return (
+                <motion.div key={index} initial={{opacity:0,height:0}} animate={{opacity:1, height:"auto"}} exit={{opacity:0, height:0}}>
+                  <FormField
+                    key={index}
+                    control={form.control}
+                    name={`units.${index}`}
+                    render={({ field }) => {
+                      return (
+                        <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
+                          <FormLabel className="flex-[1] text-xl ">
+                            Unit {index + 1}
+                          </FormLabel>
+                          <FormControl className="flex-[6]">
+                            <Input
+                              className="flex-[6]"
+                              placeholder="Enter subtopic of the main topic"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                </motion.div>
+              );
+            })}
+          </AnimatePresence>
 
           {/* Form Buttons: Add & Remove Unit */}
           <div className="flex items-center justify-center mt-4">
