ab2131394d1de2cf363297ed7a94d03858c0e2d9
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-24 07:21:25 +0100

    Add Sign In component, other links to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import SignInButton from "./SignInButton";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <nav className='fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2'>
-        <div className='flex items-center justify-center h-full gap-2 px-8 mx-auto sm-justify-between max-w-7xl'>
-            {/* logo. hidden on small screens, shown and larger screens */}
-            <Link href={"/gallery"} className="items-center hidden gap-2 sm:flex">
-            <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
-                Learning.AI
-            </p>
+    <nav className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
+      <div className="flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl">
+        {/* logo. hidden on small screens, shown and larger screens */}
+        <Link href={"/gallery"} className="items-center hidden gap-2 sm:flex">
+          <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
+            Learning.AI
+          </p>
+        </Link>
+        <div className="flex items-center">
+            {/* create course link */}
+            <Link href={"/create"} className="mr-3">
+                Create course
             </Link>
-            
-            
+            {/* Setting page link */}
+            <Link href={"/settings"} className="mr-3">
+                Settings
+            </Link>
+            <SignInButton/>
 
         </div>
-
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
