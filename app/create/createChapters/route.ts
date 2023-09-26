47d8a7e042b5babfa212f17e863a67f870460d3c
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-26 06:58:49 +0100

    Add OpenAI GPT, strict JSON output modification

--- app/create/createChapters/route.ts
+++ app/create/createChapters/route.ts
@@ -1,3 +1,4 @@
+import { strict_output } from "@/lib/gpt";
 import { createChapterSchema } from "@/validators/course";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
@@ -9,14 +10,29 @@ export async function POST(req: Request, res: Response) {
     const { title, units } = createChapterSchema.parse(body);
     // create output unit type
     type outputUnits = {
-        title: string;
-        chapters: {
-            youtube_search_query: string;
-            chapter_title: string;
+      title: string;
+      chapters: {
+        youtube_search_query: string;
+        chapter_title: string;
+      };
+
+    };
+    let output_units: outputUnits = await strict_output(
+        "You are an AI capable of curating course content, coming up with relevant chapter titles, and finding relevant youtube videos for each chapter",
+        new Array(units.length).fill(
+          `It is your job to create a course about ${title}. The user has requested to create chapters for each of the units. Then, for each chapter, provide a detailed youtube search query that can be used to find an informative educationalvideo for each chapter. Each query should give an educational informative course in youtube.`
+        ),
+        {
+          title: "title of the unit",
+          chapters:
+            "an array of chapters, each chapter should have a youtube_search_query and a chapter_title key in the JSON object",
         }
+      );
 
+    console.log(output_units);
+    return NextResponse.json(output_units);
+    
 
-    }
   } catch (error) {
     if (error instanceof ZodError) {
       return new NextResponse(
@@ -25,5 +41,4 @@ export async function POST(req: Request, res: Response) {
       );
     }
   }
-  return NextResponse.json({ hello: "hello world" });
 }
