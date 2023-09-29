6897ce2bcd715983a3a581bda8d96b3bc2c8c96d
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-29 18:55:52 +0100

    Develop quiz section for the application

--- components/QuizCards.tsx
+++ components/QuizCards.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { cn } from "@/lib/utils";
 import { Chapter, Question } from "@prisma/client";
@@ -7,16 +7,88 @@ import React from "react";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { ChevronRight } from "lucide-react";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 
 type Props = {
   chapter: Chapter & {
     questions: Question[];
   };
 };
-const QuizCards = (props: Props) => {
+const QuizCards = ({ chapter }: Props) => {
+  // answer state
+  const [answers, setAnswers] = React.useState<Record<string, string>>({});
+  console.log(answers);
+  // question state stores the current question you have selected
+  const [questionState, setQuestionState] = React.useState<
+    Record<string, boolean | null>
+  >({});
+  // check answer callback
+  const checkAnswer = React.useCallback(() => {
+    const newQuestionState = { ...questionState };
+    chapter.questions.forEach((question) => {
+      const user_answer = answers[question.id];
+      if (!user_answer) return;
+      if (user_answer === question.answer) {
+        newQuestionState[question.id] = true;
+      } else {
+        newQuestionState[question.id] = false;
+      }
+      setQuestionState(newQuestionState);
+    });
+  }, [answers, questionState, chapter.questions]);
   return (
-    <div>QuizCards</div>
-  )
-}
+    <div className="flex-[1] mt-16 ml-8">
+      <h1 className="text-2xl font-bold">Concept Check</h1>
+      {/* questions */}
+      <div className="mt-2">
+        {chapter.questions.map((question, questionIndex) => {
+          const options = JSON.parse(question.options) as string[];
+          return (
+            <div
+              key={question.id}
+              className={cn("p-3 mt-4 border border-secondary rounded-lg", {
+                "bg-green-700": questionState[question.id] === true,
+                "bg-red-700": questionState[question.id] === false,
+                "bg-secondary": questionState[question.id] === null,
+              })}
+            >
+              <h1 className="text-lg font-semibold">{question.question}</h1>
+              <div className="mt-2">
+                <RadioGroup
+                  onValueChange={(event) => {
+                    setAnswers((prev) => {
+                      return {
+                        ...prev,
+                        [question.id]: event,
+                      };
+                    });
+                  }}
+                >
+                  {options.map((option, index) => {
+                    return (
+                      <div key={index} className="flex items-center space-x-2">
+                        <RadioGroupItem
+                          value={option}
+                          id={question.id + index.toString()}
+                        />
+                        <Label htmlFor={question.id + index.toString()}>
+                          {option}
+                        </Label>
+                      </div>
+                    );
+                  })}
+                </RadioGroup>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <Button className="w-full mt-2" size="lg" onClick={checkAnswer}>
+        Check Answer
+        <ChevronRight className="w-4 h-4 ml-1" />
+      </Button>
+    </div>
+  );
+};
 
-export default QuizCards
+export default QuizCards;
